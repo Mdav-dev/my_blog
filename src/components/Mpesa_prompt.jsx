7aed61e653c07dbd6d6c52825d6760239580a045
MembaDavid <davidmemba2@gmail.com>
2024-03-20 01:40:57 +0300

    Initial Setup

---
+++ src/components/Mpesa_prompt.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Mpesa_prompt() {
+  return (
+    <div>
+	 
+    </div>
+  )
+}
+
+export default Mpesa_prompt
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './components/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,4 +10,4 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
+
